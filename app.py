80fa339d8819c496d549698aa43703dc5e07f446
Alejandro Saavedra <alejandro.saavedra@medlog.com>
2024-10-30 10:09:01 -0300

    Agregar función de login báscia

---
+++ app.py
@@ -0,0 +1,9 @@
+def login(username, password):
+    # Lógica ficticia de autenticación
+    if username == "admin" and password == "1234":
+        return "Autenticación exitosa"
+    else:
+        return "Credenciales incorrectas"
+    if __name__ == "__main__":
+        # Simulación de login
+        print(login("admin", "1234"))
